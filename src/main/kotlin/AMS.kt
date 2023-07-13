1710b6b6f74304ed83bed4e4094e087e4f81a9d8
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-13 16:32:45 -0300

    Atualização do arquivo AMS e criada a função para saber se ainda pode ser adicionado peixe, função canAddFish()

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -43,6 +43,6 @@ fun fishFood(day : String) : String {
 
 }
 
-fun camAddFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true) : Boolean{
+fun canAddFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true) : Boolean{
     return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
