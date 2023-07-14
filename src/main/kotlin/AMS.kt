6cde0f9f209a3b15e323f3ec1c7d4e6f207c771a
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-14 16:35:39 -0300

    ultilizamos o método filter e funcões lambdas

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -3,6 +3,59 @@ import java.util.Random
 fun main (args: Array<String>) {
     println("Hello, ${args[0]}!")
     feedTheFish()
+    eagerExample()
+
+    var bubbles = 0
+    while (bubbles < 50) {
+        bubbles++
+    }
+
+    // repeat (function from standard library)
+    repeat(2) {
+        println("A fish swimming")
+    }
+}
+
+fun eagerExample() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "aligator", "flowerpot")
+    val eager = decorations.filter { it[0] == 'p' }
+    println(eager)
+
+    // apply filter lazily
+    val filtered = decorations.asSequence().filter { it[0] == 'p'  }
+    println(filtered)
+    println(filtered.toList())
+
+    // apply map lazily
+    val lazyMap = decorations.asSequence().map {
+        println("map: $it")
+    }
+
+    println(lazyMap)
+    println("first: ${lazyMap.first()}")
+    println("all: ${lazyMap.toList()}")
+
+}
+
+fun feedTheFish(){
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+    shouldChangeWater(day, 20 , 50 )
+    shouldChangeWater(day)
+    shouldChangeWater(day, dirty=50)
+    shouldChangeWater(day = "Monday")
+
+    if (shouldChangeWater(day)){
+        println("Change the water today")
+    }
+
+    //call dirty processor
+    dirtyProcessor()
+}
+
+fun swin() {
+    // swin!
 }
 
 fun shouldChangeWater(
@@ -22,24 +75,29 @@ fun shouldChangeWater(
     }
 }
 
+var dirty = 20
+
+val walterFilter: (Int) -> Int = { dirty -> dirty / 2}
+fun feedFish(dirty: Int) = dirty + 10
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+fun dirtyProcessor() {
+    dirty = updateDirty(dirty, walterFilter)
+    dirty = updateDirty(dirty, ::feedFish)
+    dirty = updateDirty(dirty, { dirty ->
+        dirty + 50
+    })
+}
+
 fun isTooHot (temperature: Int) = temperature > 30
 fun isDirty (dirty: Int) = dirty > 30
 fun isSunday (day: String) = day == "Sunday"
 
 
-fun feedTheFish(){
-    val day = randomDay()
-    val food = fishFood(day)
-    println("Today is $day and the fish eat $food")
-    shouldChangeWater(day, 20 , 50 )
-    shouldChangeWater(day)
-    shouldChangeWater(day, dirty=50)
-    shouldChangeWater(day = "Monday")
 
-    if (shouldChangeWater(day)){
-        println("Change the water today")
-    }
-}
 
 fun randomDay() : String {
     val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
