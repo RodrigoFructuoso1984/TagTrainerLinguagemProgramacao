f153472b22c1295a57673f67e2b8dfd0c6c68257
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-14 10:49:10 -0300

    implementamos a logica de controle de fluxo na função shouldChangeWater, utilizando a função de condição única e utilizamos na expressão when

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -9,8 +9,23 @@ fun shouldChangeWater(
     day: String,
     temperature: Int = 22,
     dirty: Int = 20) : Boolean {
-    return true
+
+    val isTooHot = temperature > 30
+    val isDirty = dirty > 30
+    val isSunday = day == "Sunday"
+
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
 }
+
+fun isTooHot (temperature: Int) = temperature > 30
+fun isDirty (dirty: Int) = dirty > 30
+fun isSunday (day: String) = day == "Sunday"
+
 
 fun feedTheFish(){
     val day = randomDay()
