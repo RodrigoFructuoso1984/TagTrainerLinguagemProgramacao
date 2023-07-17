3369ac518c2a2812c984261e50d151063c49a9d2
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-17 18:18:11 -0300

    Criação do objeto myAquarium2 e objeto smallAquarium

--- src/main/kotlin/Aquarium/main.kt
+++ src/main/kotlin/Aquarium/main.kt
@@ -3,7 +3,6 @@ package Aquarium
 fun main (args: Array<String>) {
     buildAquarium()
 
-
 }
 
 fun buildAquarium() {
@@ -17,5 +16,18 @@ fun buildAquarium() {
     myAquarium.height = 80
 
     println("Height: ${myAquarium.height} cm")
+
     println("Volume: ${myAquarium.volume} liters")
+
+    val smallAquarium = Aquarium(length = 20, height = 15, width = 30)
+    println("Small Aquarium: ${smallAquarium.volume} liters")
+
+    val myAquarium2 = Aquarium(numberOfFish = 9)
+
+    println("Small Aquarium 2: ${myAquarium2.volume} liters with " +
+            "Length ${myAquarium2.length} " +
+            "Width ${myAquarium2.width} " +
+            "height ${myAquarium2.height} "
+    )
+
 }
