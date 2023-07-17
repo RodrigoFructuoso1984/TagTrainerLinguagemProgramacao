799d981420fe06c863817a1c8c3c3ea7370572b2
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-17 18:24:58 -0300

    criei um construtor e vários init

--- src/main/kotlin/Aquarium/Fish.kt
+++ src/main/kotlin/Aquarium/Fish.kt
@@ -1,4 +1,37 @@
 package Aquarium
 
-class Fish {
+class Fish (val friendly: Boolean = true, volumeNeeded: Int) {
+    val size: Int
+
+    init {
+        println("first init block")
+    }
+
+    constructor(): this(true, 9){
+        println("running constructor")
+    }
+
+    init {
+        if (friendly) {
+            size = volumeNeeded
+        } else {
+            size = volumeNeeded * 2
+        }
+    }
+
+    init {
+        println("constructed fish of size $volumeNeeded final size ${this.size}")
+    }
+
+    init {
+        println("last init block")
+    }
+
+}
+
+fun makeDefaultFish() = Fish(true, 50)
+
+fun printFish () {
+    val fish = Fish(true, 20)
+    println("Is the fish friendly? ${fish.friendly}. It needs volume ${fish.size}")
 }
