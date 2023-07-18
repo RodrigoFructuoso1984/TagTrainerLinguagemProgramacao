a1bd8fe04ae81d9b743c8d3a0a8da5681aaf01ca
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-18 09:58:17 -0300

    Criei a função makeSalt(), criei filtros para informar o nível de picância

--- src/main/kotlin/Spice.kt
+++ src/main/kotlin/Spice.kt
@@ -1,20 +1,53 @@
 fun main(args: Array<String>){
-    val spiceList = listOf(Spice("Canela", "Media"),
-        Spice("Pimenta", "Alta"),
-        Spice("Cominho", "Baixa"),
+
+    println("Lista de especiarias:")
+    val spices = listOf(Spice("Canela", "mild"),
+        Spice("Pimenta", "spicy"),
+        Spice("Curry", "medium"),
         Spice("Cominho"))
-    val spice = spiceList.forEach{}
+
+    makeSalt()
+
+    /*val spice = spiceList.forEach{}
     //val spice = Spice(spiceList[0].name, spiceList[0].spiciness)
-    println("${spice}")
+    println("${spice}")*/
+
+    val lessSpicy = spices.filter {it.heat < 3}
+    val moreSpicy = spices.filter { it.heat >= 3 }
+
+    //lessSpicy.forEach{ println("nome - ${it.name}, Nível de Picância - ${it.spiciness}, Calor - ${it.heat}") }
+    //val spice = lessSpicy.forEach{}
+    //println("${spice}")
+
+    println("\nEspeciarias menos picantes:")
+    for (spice in lessSpicy) {
+        println("Nome: ${spice.name}, Picância: ${spice.spiciness}, Calor: ${spice.heat}")
+    }
+
+    println("\nEspeciarias mais picantes:")
+    for (spice in moreSpicy) {
+        println("Nome: ${spice.name}, Picância: ${spice.spiciness}, Calor: ${spice.heat}")
+    }
+
 }
 
 class Spice (val name: String, val spiciness: String = "mild") {
+    val heat: Int
+        get() {
+            return when(spiciness){
+                "mild" -> 1
+                "medium" -> 3
+                "spicy" -> 5
+                else -> 0
+            }
+        }
 
     init {
-        println("nome - ${name}, Nível de Picância - ${spiciness}")
+        println("Nome - ${name}, Picância - ${spiciness}, Calor - ${heat}")
     }
 
-    val heat: Int
-        get() {return 2}
+}
 
+fun makeSalt() : Spice {
+    return Spice("Salt", "mild")
 }
