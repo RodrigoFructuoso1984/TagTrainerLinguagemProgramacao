add937cde0244adfac8c847c287b5349d032fe46
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-18 18:03:35 -0300

    Copiei a classe Spice e tornei abstract, criei interface Grinder, SpiceColor, criei função abstract prepareSpice, criei object YellowSpiceColor para setar uma cor padrão, criei subclasse Curry com herança da classe Spice

--- src/main/kotlin/Spices/Spice.kt
+++ src/main/kotlin/Spices/Spice.kt
@@ -1,2 +1,34 @@
 package Spices
 
+fun main(args: Array<String>) {
+    val mildCurry = Curry("Mild Curry", "mild")
+    println("Curry color: ${mildCurry.color}")
+    mildCurry.prepareSpice()
+    mildCurry.grind()
+}
+
+abstract class Spice(val name: String, val spiciness: String = "mild", spiceColor: SpiceColor = YellowSpiceColor) : SpiceColor by spiceColor {
+    abstract fun prepareSpice()
+}
+
+interface Grinder {
+    fun grind()
+}
+
+interface SpiceColor {
+    val color: String
+}
+
+object YellowSpiceColor: SpiceColor {
+    override val color: String = "Yellow"
+}
+
+class Curry(name: String, spiciness: String) : Spice(name, spiciness), Grinder  {
+    override fun prepareSpice() {
+        println("Preparing $name curry with spiciness level: $spiciness")
+    }
+
+    override fun grind() {
+        println("Grinding $name curry")
+    }
+}
