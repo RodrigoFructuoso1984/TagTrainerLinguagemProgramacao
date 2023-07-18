306a2b7bbe6fc9ace47017bc0dd7a5fc2cb39731
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-18 16:44:40 -0300

    Criei a função makeFish para criação de peixe com cor e ação de comer

--- src/main/kotlin/Aquarium/main.kt
+++ src/main/kotlin/Aquarium/main.kt
@@ -2,6 +2,7 @@ package Aquarium
 
 fun main (args: Array<String>) {
     buildAquarium()
+    makeFish()
 
 }
 
@@ -30,4 +31,19 @@ fun buildAquarium() {
             "height ${myAquarium2.height} "
     )
 
+}
+
+fun feedFish(fish: FishAction) {
+    // make some food then
+    fish.eat()
+}
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+
+    println("Shark: ${shark.color} \n Plecostomus: ${pleco.color}")
+
+    shark.eat()
+    pleco.eat()
 }
