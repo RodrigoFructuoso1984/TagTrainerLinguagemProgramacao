a078385d3d45297f044a86e895e689cc6b5e55de
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-19 18:02:37 -0300

    Criei o packge Buildings, e o arquivo BaseBuildingMaterial.kt, dentro dele criei a class BaseBuildingMaterial e sub classes Wood e Brick, para simular construção

--- src/main/kotlin/Buildings/BaseBuildingMaterial.kt
+++ src/main/kotlin/Buildings/BaseBuildingMaterial.kt
@@ -1,2 +1,15 @@
 package Buildings
 
+open class BaseBuildingMaterial {
+    open val numberNeeded: Int = 1
+}
+
+class Wood: BaseBuildingMaterial() {
+    override val numberNeeded: Int = 4
+}
+class Brick: BaseBuildingMaterial() {
+    override val numberNeeded: Int = 8
+}
+
+
+
