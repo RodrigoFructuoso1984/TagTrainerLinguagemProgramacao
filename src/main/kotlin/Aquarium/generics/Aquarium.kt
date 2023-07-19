263cf239a0045f375a4018fe92a082adbd5fd538
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-19 17:59:23 -0300

    Criei o package generics para fazer a atividade dos eventos genericos

--- src/main/kotlin/Aquarium/generics/Aquarium.kt
+++ src/main/kotlin/Aquarium/generics/Aquarium.kt
@@ -1,2 +1,42 @@
 package Aquarium.generics
 
+fun main(args: Array<String>){
+    genericExample()
+}
+
+open class WaterSupply(var needsProcessed: Boolean)
+
+
+class TapWater : WaterSupply(needsProcessed = true) {
+    fun addChemicalCleaners() {
+        needsProcessed = false
+    }
+}
+
+class FishStoreWater : WaterSupply(needsProcessed = false) {}
+
+class LakeWater : WaterSupply(needsProcessed = true) {
+    fun filter() {
+        needsProcessed = false
+    }
+}
+
+class Aquarium<T: WaterSupply>(val waterSupply: T) {
+    fun addWater() {
+        check(!waterSupply.needsProcessed) {"Water supply needs processed"}
+
+        println("adding water from $waterSupply")
+    }
+}
+
+fun genericExample() {
+
+    val aquarium: Aquarium<TapWater> = Aquarium(TapWater())
+    aquarium.waterSupply.addChemicalCleaners()
+
+    val aquarium4: Aquarium<LakeWater> = Aquarium(LakeWater())
+    aquarium4.waterSupply.filter()
+    aquarium4.addWater()
+
+}
+
