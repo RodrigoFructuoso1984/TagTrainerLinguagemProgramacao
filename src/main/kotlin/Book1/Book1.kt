ff00e0dad7f9fa4d7222aad7826e4ebb98316356
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-19 14:01:07 -0300

    Manupulação de list, mutableList, map, e mutableLMap

--- src/main/kotlin/Book1/Book1.kt
+++ src/main/kotlin/Book1/Book1.kt
@@ -12,14 +12,35 @@ class Book(val title: String, val author: String, val year: Int) {
 }
 
 fun main(args: Array<String>) {
-    val book = Book("A Guerra dos Tronos", "George R. R. Martin", 1996)
+    /*val book = Book("A Guerra dos Tronos", "George R. R. Martin", 1996)
 
     val titleAuthorYearTriple = book.titleWithAuthorWithYearTriple()
     val (nome, author, title) = titleAuthorYearTriple
-    println(nome)git
+    println(nome)
     println(author)
     println(title)
     println("Aqui está o livro ${titleAuthorYearTriple.first}," +
             " escrito por ${titleAuthorYearTriple.second}" +
-            " em ${titleAuthorYearTriple.third}")
+            " em ${titleAuthorYearTriple.third}")*/
+
+    val allBooks: MutableList<String> = mutableListOf("Romeo and Juliet", "Hamlet", "Macbeth", "Othello")
+    val library: Map<String, MutableList<String>> = mapOf("William Shakespeare" to allBooks)
+    //println(library.get("William Shakespeare"))
+    //library.contains("Hamlet")
+    //library.any { it.value.contains("Hamlet") }
+
+    val isHamlet = library.any { it.value.contains("Hamlet") }
+    println("Is there any book called Hamlet? $isHamlet")
+
+    val moreBooks = mutableMapOf<String, String>()
+    moreBooks["The Tempest"] = "William Shakespeare"
+
+    val title = "Hamlet"
+    //val title = "The Tempest"
+
+    if (!moreBooks.containsKey(title)) {
+        moreBooks[title] = "William Shakespeare"
+    }
+
+    println("Teste" + moreBooks)
 }
