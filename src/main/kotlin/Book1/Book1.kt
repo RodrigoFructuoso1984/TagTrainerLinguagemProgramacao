b8e085258bfd8ddc056dcf50d9419a25b1e3d751
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-19 11:15:01 -0300

    Utilizamos Pair e Triple para retornar propriedades do objeto Book

--- src/main/kotlin/Book1/Book1.kt
+++ src/main/kotlin/Book1/Book1.kt
@@ -1,5 +1,25 @@
-package Book
+package Book1
 
-class Book(val title: String, val author: String, val yerar: String) {
+class Book(val title: String, val author: String, val year: Int) {
+    fun titleWithAuthorPair(): Pair<String,String>{
+        return  title to author
+    }
 
+    fun titleWithAuthorWithYearTriple(): Triple<String, String, Int>{
+        return Triple(title, author, year)
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val book = Book("A Guerra dos Tronos", "George R. R. Martin", 1996)
+
+    val titleAuthorYearTriple = book.titleWithAuthorWithYearTriple()
+    val (nome, author, title) = titleAuthorYearTriple
+    println(nome)git
+    println(author)
+    println(title)
+    println("Aqui está o livro ${titleAuthorYearTriple.first}," +
+            " escrito por ${titleAuthorYearTriple.second}" +
+            " em ${titleAuthorYearTriple.third}")
 }
