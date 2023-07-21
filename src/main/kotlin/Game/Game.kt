2985a1a671aea178e3677c6819af1b6b7a69a8b6
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-21 14:42:25 -0300

    utilizei map para mapear movimentos, foi criado uma classe location, e metodo updateLocation

--- src/main/kotlin/Game/Game.kt
+++ src/main/kotlin/Game/Game.kt
@@ -1,11 +1,46 @@
 package Game
+import kotlin.math.absoluteValue
 
 enum class Directions {
     NORTH, SOUTH, EAST, WEST, START, END
 }
 
+class Location (val width: Int = 4, val heigth: Int = 4){
+
+    val map = Array(width) { arrayOfNulls<String>(heigth) }
+
+    var currentLocation = Pair (0,0)
+
+    fun updateLocation(direction: Directions) {
+
+        if(direction.equals(Directions.NORTH)) {
+            currentLocation = Pair(currentLocation.first, (currentLocation.second + 1).rem(heigth))}
+        else if(direction.equals(Directions.SOUTH)) {
+            currentLocation = Pair(currentLocation.first, (currentLocation.second - 1).absoluteValue.rem(heigth))}
+        else if(direction.equals(Directions.EAST)) {
+            currentLocation = Pair((currentLocation.first + 1).rem(width), currentLocation.second)}
+        else if(direction.equals(Directions.WEST)) {
+            currentLocation = Pair((currentLocation.first - 1).absoluteValue.rem(width), currentLocation.second)}
+    }
+
+    fun getDescription ():String? {
+        return map[currentLocation.first.rem(width)][currentLocation.second.rem(heigth)]
+    }
+    init {
+        for (x in 0 until width){
+            for (y in 0 until heigth){
+                map[x][y] = "Localização $x - $y"
+            }
+        }
+    }
+
+}
+
+
 class Game {
     val path: MutableList<Directions> = mutableListOf(Directions.START)
+    val map = Location()
+
     val north = {path.add(Directions.NORTH)}
     val south = {path.add(Directions.SOUTH)}
     val east = {path.add(Directions.EAST)}
@@ -25,11 +60,11 @@ class Game {
         where.invoke()
     }
 
-    fun makeMove(command : String?) {
-        if (command.equals("n")) move(north)
-        else if (command.equals("s")) move(south)
-        else if (command.equals("e")) move(east)
-        else if (command.equals("w")) move(west)
+    fun makeMove(direction: String?) {
+        if (direction.equals("n"))  { move(north); map.updateLocation(Directions.NORTH) }
+        else if (direction.equals("s")) { move(south); map.updateLocation(Directions.SOUTH) }
+        else if (direction.equals("e")) { move(east); map.updateLocation(Directions.EAST) }
+        else if (direction.equals("w")) { move(west); map.updateLocation(Directions.WEST) }
         else move(end)
     }
 
@@ -41,6 +76,7 @@ fun main(args: Array<String>) {
     while (true) {
         print("Enter a direction - n/s/e/w ou f[encerrar]: ")
         game.makeMove(readLine())
+        println(game.map.getDescription())
     }
 
 
