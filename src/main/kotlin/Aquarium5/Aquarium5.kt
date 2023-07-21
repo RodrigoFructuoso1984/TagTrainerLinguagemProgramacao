bb3ff22827276c9f347bcb032491a79a1ae9bca1
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-21 10:14:10 -0300

    utilizamos funções de ordem superior

--- src/main/kotlin/Aquarium5/Aquarium5.kt
+++ src/main/kotlin/Aquarium5/Aquarium5.kt
@@ -1,2 +1,43 @@
 package Aquarium5
 
+data class Fish(var name: String)
+
+fun main ( args: Array<String> ) {
+    fishExamples()
+    val numbers = listOf<Int>(1,2,3,4,5,6,7,8,9,0)
+    print(numbers.divisibleBy {
+        it.rem(3)
+    })
+}
+
+fun List<Int>.divisibleBy(lambda: (Int) -> Int): List<Int> {
+    val numbers = mutableListOf<Int>()
+    for (item in this){
+        if (lambda(item) == 0){
+            numbers.add(item)
+        }
+    }
+    return numbers
+}
+
+fun fishExamples() {
+    val fish = Fish("splashy")
+
+    myWith (fish.name){
+        println(capitalize())
+    }
+    println(fish.run { name })
+    println(fish.apply {  })
+
+    val fish2:Fish = Fish(name = "splashy").apply { name = "Sharky" }
+    println(fish2.name)
+
+    println(fish.let { it.name.capitalize() }
+        .let { it + "fish" }
+        .let { it.length }
+        .let { it + 31 })
+}
+
+fun myWith(name: String, block: String.() -> Unit) {
+    name.block()
+}
