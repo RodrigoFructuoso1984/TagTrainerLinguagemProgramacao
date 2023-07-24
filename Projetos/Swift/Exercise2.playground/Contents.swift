52de6b0797adefcd9326fc6743229165a5b75f5f
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-24 14:12:53 -0300

    exercicio 2 - nomear as variaveis conforme as regras

---
+++ Projetos/Swift/Exercise2.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+/*
+Write it correctly:
+
+var Username
+
+var Userinput
+
+let databaseadress
+
+var resultfromcalculate
+*/
+
+var username = ""
+var userInput = ""
+let databaseAdress = ""
+var resultFromCalculate = ""
