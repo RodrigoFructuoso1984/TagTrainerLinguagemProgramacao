39cf03ee96aa3a9074bf08b374e06992d42cf444
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-24 10:59:15 -0300

    conteúdo sobre var e let

---
+++ Projetos/Swift/SwiftBasic.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+/*
+var greeting = "Hello, playground"
+*/
+
+// var -> variable -> because the value can be changed
+// let -> constant -> the value can not be changed, after the assigment
+
+var str = "Hello, playground"
+print(str)
+str = "Hello!"
+print(str)
+
+var fisrtName = "john"
+var email = "john@gmail.com"
+var password = "1234"
+
+email = "john@work.com"
+password = "lsdoii234"
+var lifePoint = 100
+lifePoint = 80
+var lifeCount = 3
+lifeCount = 2
+lifeCount = 1
+var playerImage = "Image of Spaceship"
+
+let databaseAdress = "database123"
