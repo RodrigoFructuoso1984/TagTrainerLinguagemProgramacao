c82c6f723f8a61c5a7ee5aea4148fbe809e4807f
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-24 14:17:07 -0300

    exercise3 - dados os nomes de variaveis e os valores defini o tipo de cada variavel

---
+++ Projetos/Swift/Exercise3.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+/*
+ Add the data type
+
+ var value = 23454
+
+ var value1 = "I'm from germany"
+
+ var value2 = 30.345
+ */
+
+var value: Int = 23454
+var valu1: String = "I'm from germany"
+var value2: Double = 30.345
