8e38cb36cb102346c9d5f7021d7f303e75daf576
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-25 16:24:21 -0300

    exercise2 if else

---
+++ Projetos/Swift/Exercise2IfElse.playground/Contents.swift
@@ -0,0 +1,10 @@
+var musicOnOff: Bool = false
+
+func checkMusiconOff (musicOnOff: Bool){
+    if musicOnOff == true {
+        print("musicOn")
+    } else {
+        print("musicOff")
+    }
+}
+ checkMusiconOff(musicOnOff: musicOnOff)
