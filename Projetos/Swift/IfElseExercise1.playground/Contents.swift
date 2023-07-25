be233ba48763c3c12013477c9dd803ca6149356e
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-25 16:16:59 -0300

    exercise1 if else

---
+++ Projetos/Swift/IfElseExercise1.playground/Contents.swift
@@ -0,0 +1,13 @@
+var difficult = "hard"
+
+var numberOfEnemy = 0
+
+if difficult == "easy" {
+    numberOfEnemy = 10
+} else if difficult == "medium" {
+    numberOfEnemy = 20
+} else if difficult == "hard" {
+    numberOfEnemy = 30
+}
+
+print(numberOfEnemy)
