290fa8981ec697dea420d3e17ba7266ea24a5c3c
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-25 14:33:46 -0300

    create outlets e create action e create func calculate(), func createAMessageWindow(message: String), func printUIElements()

--- Tip-Calculator/ViewController.swift
+++ Tip-Calculator/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var personsCountTextField: UITextField!
+    
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var percentSlider: UISlider!
+    
+    @IBOutlet weak var tipCostLabel: UILabel!
+    @IBOutlet weak var totalCostLabel: UILabel!
+    @IBOutlet weak var costPerPersonLabel: UILabel!
+    
+    var foodCost = 0.0
+    var tipPrecent = 0.0
+    var tipCost = 0.0
+    var totalCost = 0.0
+    var costPerPerson = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func precentSlider(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func precentButton(_ sender: UIButton) {
+        print(sender.titleLabel?.text)
+    }
+    
+    func calculate(){}
+    
+    func createAMessageWindow(message:String){}
+    
+    func printUIElements(){}
+    
 }
 
