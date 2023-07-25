09fe9097284a4c8dca162e383574ed5fb795fb98
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-25 16:40:43 -0300

    exercise4 if else

---
+++ Projetos/Swift/Exercise4IfElse.playground/Contents.swift
@@ -0,0 +1,55 @@
+/*
+ var number = 20
+
+if number == 20 {
+    print("Equal")
+} else {
+    print("Not equal")
+}
+// print("Equal") Because 20 equal 20
+*/
+var number = 10
+
+if number != 20 {
+    print("Not Equal")
+} else {
+    print("Equal")
+}
+ // print("Not Equal") Because 10 is NOT 20
+ /*
+var number = 9
+
+if number < 20 {
+    print("number is smaller than 20")
+} else {
+    print("number is bigger than 20")
+}
+ // print("number is smaller than 20") Because 9 is smaller than 20
+
+var number = 20
+
+if number > 20 {
+    print("number is bigger than 20")
+} else {
+    print("number is smaller than 20")
+}
+ // print("number is smaller than 20") Because 20 is not bigger than 20
+
+var number = 19
+
+if number <= 20 {
+    print("number is smaller or equal to 20")
+} else {
+    print("number is bigger than 20")
+}
+ // print("number is smaller or equal to 20") Because 19 ist smaller than 20
+
+var number = 20
+
+if number >= 20 {
+    print("number is bigger or equal to 20")
+} else {
+    print("number is smaller or equal to 20")
+}
+ //  print("number is bigger or equal to 20") Because 20 is equal to 20
+*/
