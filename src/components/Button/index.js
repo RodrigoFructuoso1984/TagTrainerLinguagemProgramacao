31457993123407fb1bc39e99d97c6cabbc525057
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-27 17:27:42 -0300

    Vimos os componentes utilizando o modo Stateful e Stateless no componente Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import { Text,View, TouchableOpacity, Alert } from 'react-native';
 import PropTypes from 'prop-types';
 
-/*
+// Stateless
 const Button = () => {
 
   return (
-    <View>
-        <Text>Button</Text>
-    </View>
+    <TouchableOpacity
+        onPress={ ()  =>  {
+            Alert.alert(this.props.alerta);
+        }}
+    >
+         {this.props.children}
+    </TouchableOpacity>
   );
 }
 
-export default Button;
-*/
+Button.propTypes = {
+    alerta: PropTypes.string
+}
 
+Button.defaultProps = {
+    alerta: "Valor padrao"
+}
+
+export default Button;
+
+// Stateful
+/*
 export default class Button extends Component {
 
     static propTypes = {
@@ -38,3 +51,4 @@ export default class Button extends Component {
         )
     }
 }
+*/
